75bb48fd55cb51a5be55864f07b5347fd8278696
otus-dn-core <otus@dn-core.ru>
2021-09-14 20:44:09 +0300

    tag.entity.ts !!! synchronize: true !!! TEST

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,6 +7,8 @@ const config: ConnectionOptions = {
   username: 'test_sm',
   password: 'admin',
   database: 'test_sm',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
 };
 
 export default config;
